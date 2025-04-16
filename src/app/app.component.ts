aa7b6568a349e59ae92196fa146d95f5756ed6cf
Steven <gringostar4@gmail.com>
2025-04-15 20:18:45 -0500

    updated padre component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { PadreComponent } from "./padre/padre.component";
 
 @Component({
   selector: 'app-root',
-  imports: [PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
